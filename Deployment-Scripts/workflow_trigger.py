caf4cb797a18844c56fe5a0ae266989b48c113e6
Ahmed <52006154+ahmedb24@users.noreply.github.com>
2022-11-23 01:10:36 +0100

    Update workflow_trigger.py

--- Deployment-Scripts/workflow_trigger.py
+++ Deployment-Scripts/workflow_trigger.py
@@ -7,9 +7,8 @@ TOKEN= str(sys.argv[1])
 OWNER= str(sys.argv[2])
 REPO= str(sys.argv[3])
 Workflow_Name= str(sys.argv[4])
-print("The Print out areeeeeeeeee ",sys.argv, sys.argv[5], sys.argv[6])
 pl_Baseline_Number= str(sys.argv[5])
-pl_Baseline_Revision = str(sys.argv[6])
+pl_Baseline_Revision = str(sys.argv[5])
 
 
 print( "the toke value is")
